8cfc24f93b42d54fdd4c7e545cdbfae1df714a44
Beatriz Santos <beatrizsantos@MacBook-Pro-de-Beatriz-Santos.local>
2021-09-25 14:22:12 -0300

    primeira parte objeto

---
+++ exercicios/objetos.js
@@ -0,0 +1,68 @@
+
+
+
+let gata = {
+    nome: "Katina", // o atributo seria como as variaveis
+    raca: "Vira-lata",
+    peso: "4kg",
+    tutura: "Bea",
+
+
+    // esse objeto aqui
+    dizerMeuNome(nome) { // o metodo seria como uma funcao
+        console.log(this.nome);
+    }
+
+
+}
+
+gata.nome = "Kat"
+gata.dizerMeuNome()
+
+
+
+const cartao = {
+    nome: "Beatriz",
+    numero: "1234-5687",
+
+    obterNome() {
+        console.log(this.nome)
+    }
+}
+const cartaoOnline = {
+    nome: "Beatriz",
+    numero: "4687-5687",
+
+    obterNome() {
+        console.log(this.nome)
+    }
+}
+
+cartao.obterNome()
+
+
+const client = {
+    nome: "Beatriz",
+    cpf: 123456789,
+
+    endereco: {
+        rua: "Leticia",
+        cep: 1234567
+    },
+    cartoes: [
+        {
+            nome: "Beatriz",
+            numero: "1234-5687",
+        },
+        {
+            nome: "Outro",
+            numero: "1234-5687",
+        }
+    ]
+}
+
+
+console.log(client.cpf)
+console.log(client.endereco.rua)
+console.log(client["endereco"]["rua"])
+console.log(client.cartoes[1].nome)
